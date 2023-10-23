ed727a7251fc6fb35aca93b28c287fbd96fbe919
Dmitriy Kozyrev <dmitry.kozyrev4@gmail.com>
2023-10-23 22:52:55 +0200

    bot is up and running without commands

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
+import time
+
 import discord
 from discord.ext import commands
 import asyncio
 
-
-token = "MTE2NjEwMzkyMjEzMDMwMTA1OQ.GbXgkJ.UL7cim566TWVYrq5SHWYL8TEGfEad-sw11JG6k"
-prefix = "!"
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix=prefix, intents=intents)
+import settings
 
 
-@bot.event()
-async def on_ready():
-    print(f"Logged in as {bot.user}")
+def run():
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    bot = commands.Bot(command_prefix="!", intents=intents)
+
+    @bot.event
+    async def on_ready():
+        print(f"{bot.user.id} is loading...")
+        time.sleep(2)
+        print(f"Logged in as {bot.user}")
+
+    bot.run(settings.DISCORD_API_SECRET)
+
+
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
