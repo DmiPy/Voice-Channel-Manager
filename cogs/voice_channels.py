bf1dee7f97b45b48d66d3fa5bc5d493f4b71229f
Dmitriy Kozyrev <dmitry.kozyrev4@gmail.com>
2023-10-30 11:27:50 +0100

    vc can be deleted after inactivity

--- cogs/voice_channels.py
+++ cogs/voice_channels.py
@@ -3,15 +3,20 @@ import time
 import discord
 from discord.ext import commands, tasks
 
-temporary_channels = {}
+class TemporaryChannel:
+    def __init__(self, channel_id, created_at):
+        self.channel_id = channel_id
+        self.created_at = created_at
 
+temporary_channels = {}
 
 class VoiceChannels(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.check_temporary_channels.start()
 
     @commands.command()
-    async def create_time_channel(self, ctx):
+    async def create_temp_channel(self, ctx):
         guild = ctx.guild
         users = ctx.message.mentions
 
@@ -26,18 +31,24 @@ class VoiceChannels(commands.Cog):
             overwrites[user] = discord.PermissionOverwrite(read_messages=True, connect=True)
 
         channel = await guild.create_voice_channel("Временный", overwrites=overwrites)
-        temporary_channels[channel.id] = time.time()
+        temporary_channels[channel.id] = TemporaryChannel(channel.id, time.time())
 
-    @tasks.loop(seconds=5)
+    @tasks.loop(minutes=1)
     async def check_temporary_channels(self):
-        # write a function that deletes temporary channels if they are empty and older than 5 minutes and 10 seconds have passed since all members left
-        for channel_id, time_created in temporary_channels.items():
-            if time.time() - time_created > 5 * 60 + 10:
+        # delete voice channels that are older than 1 minute and have no users in them
+        channels_to_delete = []
+
+        for channel_id, temp_channel in temporary_channels.copy().items():
+            if time.time() - temp_channel.created_at > 60:
                 channel = self.bot.get_channel(channel_id)
-                if channel is not None:
-                    if len(channel.members) == 0:
-                        await channel.delete()
-                        temporary_channels.pop(channel_id)
+                if channel and len(channel.members) == 0:
+                    channels_to_delete.append(channel)
+
+        for channel in channels_to_delete:
+            await channel.delete()
+            del temporary_channels[channel.id]
+            print(f"Канал {channel.name} был удален.")
+
 
 
 async def setup(bot):
--- main.py
+++ main.py
@@ -22,11 +22,22 @@ def run():
         time.sleep(2)
         print(f"Logged in as {bot.user}")
         await bot.load_extension("cogs.voice_channels")
+
+    @bot.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send("Тебе по ебалу дать? Такой команды нет...")
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Хоть мои пальцы и разбросаны по всему миру, но даже я могу пересчитать их и понять, что ты забыл ввести аргументы, додик...")
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.send("Ты думаешь, что я дурак? У тебя нет прав на это, щенок...")
+
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     run()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
