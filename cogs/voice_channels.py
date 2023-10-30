9d66adce7a5265d41a18b113f1ec666c4ec2d89b
Dmitriy Kozyrev <dmitry.kozyrev4@gmail.com>
2023-10-30 12:28:20 +0100

    tempchannel is fully functioning

--- cogs/voice_channels.py
+++ cogs/voice_channels.py
@@ -3,34 +3,49 @@ import time
 import discord
 from discord.ext import commands, tasks
 
+
 class TemporaryChannel:
     def __init__(self, channel_id, created_at):
         self.channel_id = channel_id
         self.created_at = created_at
 
+
 temporary_channels = {}
+
 
 class VoiceChannels(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.check_temporary_channels.start()
 
-    @commands.command()
-    async def create_temp_channel(self, ctx, channel_name: str):
+    @commands.command(brief="Создает временный войс чат: <название> <упоминания участников>")
+    # change function so, that it takes a name for a channel and then takes a list of mentions of users
+    async def tempchannel(self, ctx, channel_name: str, *users: discord.Member):
+        """
+            Создает временный голосовой канал на сервере Discord.
+            Параметры:
+                - `channel_name`: Название временного канала.
+                - `users`: Упомянутые участники, которые имеют доступ к каналу.
+            Пример использования:
+              !create_temp_channel TemporaryChannel @user1 @user2
+            """
+        if channel_name.startswith("<@"):
+            channel_name = "Посиделки🍿📺😏"
         guild = ctx.guild
-        users = ctx.message.mentions
-
-        print(users)
 
         overwrites = {
             guild.default_role: discord.PermissionOverwrite(read_messages=True, connect=False),
             guild.me: discord.PermissionOverwrite(read_messages=True, connect=True)
         }
-
+        usernames = [user.name for user in users]
         for user in users:
             overwrites[user] = discord.PermissionOverwrite(read_messages=True, connect=True)
 
-        channel = await guild.create_voice_channel(channel_name, overwrites=overwrites)
+        mention_users = ', '.join(user.mention for user in users)
+        await ctx.send(f"Канал '{channel_name}' был создан {ctx.author.name}. Заходите {mention_users}!")
+        time.sleep(5)
+        channel = await guild.create_voice_channel(f"{channel_name}", overwrites=overwrites)
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"Канал создан {ctx.author.name}"))
         temporary_channels[channel.id] = TemporaryChannel(channel.id, time.time())
 
     @tasks.loop(minutes=1)
@@ -47,7 +62,7 @@ class VoiceChannels(commands.Cog):
         for channel in channels_to_delete:
             await channel.delete()
             del temporary_channels[channel.id]
-            print(f"Канал {channel.name} был удален.")
+            print(f"Канал '{channel.name}' был удален.")
             # Отправка сообщения в текстовый канал
             text_channel = self.bot.get_channel(1163053302171324458)
             if text_channel:
